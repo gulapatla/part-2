8c75db6f44428d38b5f49a11e8ea029e8fa9f8e7
gulapatla <76089049+gulapatla@users.noreply.github.com>
2020-12-16 20:33:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,193 @@
+ var i = 0;
+
+ function a() {
+     document.getElementById("hohe").innerHTML = ("That's Right")
+     i++
+ }
+
+ function b() {
+     document.getElementById("hohe").innerHTML = ("That's Wrong")
+ }
+
+ function c() {
+     document.getElementById("hohe").innerHTML = ("That's Wrong")
+ }
+
+ function d() {
+     document.getElementById("hohe").innerHTML = ("That's Wrong")
+ }
+
+ function e() {
+     document.getElementById("hohe2").innerHTML = ("That's Right")
+     i++
+ }
+
+ function f() {
+     document.getElementById("hohe2").innerHTML = ("That's Wrong")
+ }
+
+ function g() {
+     document.getElementById("hohe2").innerHTML = ("That's Wrong")
+ }
+
+ function h() {
+     document.getElementById("hohe2").innerHTML = ("That's Wrong")
+ }
+
+ function i() {
+     document.getElementById("hohe3").innerHTML = ("That's Wrong")
+ }
+
+ function j() {
+     document.getElementById("hohe3").innerHTML = ("That's Wrong")
+ }
+
+ function k() {
+     document.getElementById("hohe3").innerHTML = ("That's Right")
+     i++
+ }
+
+ function l() {
+     document.getElementById("hohe3").innerHTML = ("That's Wrong")
+ }
+
+ function m() {
+     document.getElementById("hohe4").innerHTML = ("That's Wrong")
+ }
+
+ function n() {
+     document.getElementById("hohe4").innerHTML = ("That's Right")
+     i++
+ }
+
+ function o() {
+     document.getElementById("hohe4").innerHTML = ("That's Wrong")
+ }
+
+ function p() {
+     document.getElementById("hohe4").innerHTML = ("That's Wrong")
+ }
+
+ function q() {
+     document.getElementById("hohe5").innerHTML = ("That's Wrong")
+ }
+
+ function r() {
+     document.getElementById("hohe5").innerHTML = ("That's Right")
+     i++
+ }
+
+ function s() {
+     document.getElementById("hohe5").innerHTML = ("That's Wrong")
+ }
+
+ function t() {
+     document.getElementById("hohe5").innerHTML = ("That's Wrong")
+ }
+
+ function u() {
+     document.getElementById("hohe6").innerHTML = ("That's Right")
+     i++
+ }
+
+ function v() {
+     document.getElementById("hohe6").innerHTML = ("That's Wrong")
+ }
+
+ function w() {
+     document.getElementById("hohe6").innerHTML = ("That's Wrong")
+ }
+
+ function x() {
+     document.getElementById("hohe6").innerHTML = ("That's Wrong")
+ }
+
+ function y() {
+     document.getElementById("hohe7").innerHTML = ("That's Right")
+     i++
+ }
+
+ function z() {
+     document.getElementById("hohe7").innerHTML = ("That's Wrong")
+ }
+
+ function aa() {
+     document.getElementById("hohe7").innerHTML = ("That's Wrong")
+ }
+
+ function ab() {
+     document.getElementById("hohe7").innerHTML = ("That's Wrong")
+ }
+
+ function ac() {
+     document.getElementById("hohe8").innerHTML = ("That's Wrong")
+ }
+
+ function ad() {
+     document.getElementById("hohe8").innerHTML = ("That's Wrong")
+ }
+
+ function ae() {
+     document.getElementById("hohe8").innerHTML = ("That's Right")
+     i++
+ }
+
+ function af() {
+     document.getElementById("hohe8").innerHTML = ("That's Wrong")
+ }
+
+ function ag() {
+     document.getElementById("hohe9").innerHTML = ("That's Wrong")
+ }
+
+ function ah() {
+     document.getElementById("hohe9").innerHTML = ("That's Wrong")
+ }
+
+ function ai() {
+     document.getElementById("hohe9").innerHTML = ("That's Right")
+     i++
+ }
+
+ function aj() {
+     document.getElementById("hohe9").innerHTML = ("That's Wrong")
+ }
+
+ function ak() {
+     document.getElementById("hohe10").innerHTML = ("That's Wrong")
+ }
+
+ function al() {
+     document.getElementById("hohe10").innerHTML = ("That's Right")
+     i++
+ }
+
+ function am() {
+     document.getElementById("hohe10").innerHTML = ("That's WRONG")
+ }
+
+ function an() {
+     document.getElementById("hohe10").innerHTML = ("That's Wrong")
+ }
+
+ function hh() {
+     if (i > 10)
+         document.getElementById("pppopo").innerHTML = ("You might have multiple times clicked on the right answer. Please reload for a fair score!")
+     if (i <= 10)
+         document.getElementById("pppopo").innerHTML = (i);
+
+
+
+ }
+
+ function WhyAreWeStillHereJustToSuffer() {
+     
+     window.location = "https://gulapatla.github.io/WhyAreWeStillHere...JustToSuffer/."
+     
+     
+     
+     
+     
+     
+ }
+       
